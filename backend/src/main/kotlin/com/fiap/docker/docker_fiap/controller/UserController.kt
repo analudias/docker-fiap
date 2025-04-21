36babe449cbf1b3c8a5155b882ad2950d987c720
Ana Luísa Dias <analudias@live.com>
2025-04-21 20:48:40 -0300

    feature: Create, update and delete users

--- backend/src/main/kotlin/com/fiap/docker/docker_fiap/controller/UserController.kt
+++ backend/src/main/kotlin/com/fiap/docker/docker_fiap/controller/UserController.kt
@@ -2,10 +2,9 @@ package com.fiap.docker.docker_fiap.controller
 
 import com.fiap.docker.docker_fiap.entity.User
 import com.fiap.docker.docker_fiap.service.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
@@ -13,4 +12,25 @@ class UserController(private val userService: UserService) {
 
     @GetMapping
     fun findAllUsers(): ResponseEntity<List<User>> = ResponseEntity.ok(userService.findAllUsers())
+
+    @PostMapping
+    fun createUser(@RequestBody user: User): ResponseEntity<User>{
+        val savedUser = userService.createUser(user)
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser)
+    }
+
+    @PutMapping("/{id}")
+    fun updateUserById(
+        @PathVariable id: Long,
+        @RequestBody updatedUser: User
+    ): ResponseEntity<User> =
+        userService.updateUser(id, updatedUser)
+            ?.let { ResponseEntity.ok(it) }
+            ?:ResponseEntity.notFound().build()
+
+    @DeleteMapping("/{id}")
+    fun deleteUserById(@PathVariable id: Long): ResponseEntity<Void> =
+        userService.deleteById(id)
+            ?.let { ResponseEntity.noContent().build() }
+            ?: ResponseEntity.notFound().build()
 }
--- backend/src/main/kotlin/com/fiap/docker/docker_fiap/service/UserService.kt
+++ backend/src/main/kotlin/com/fiap/docker/docker_fiap/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.fiap.docker.docker_fiap.service
 
+import com.fiap.docker.docker_fiap.entity.User
 import com.fiap.docker.docker_fiap.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -9,5 +10,30 @@ class UserService(
 ) {
 
     fun findAllUsers() = userRepository.findAll()
+
+    fun createUser(user: User) = userRepository.save(user)
+
+    fun updateUser(id: Long, updatedUser: User): User? {
+        return userRepository.findById(id).orElse(null)
+            ?.let { existingUser ->
+                val userToUpdate = existingUser.copy(
+                    name = updatedUser.name,
+                    email = updatedUser.email,
+                    birthDate = updatedUser.birthDate,
+                    address = updatedUser.address
+                )
+                userRepository.save(userToUpdate)
+            }
+    }
+
+    fun deleteById(id: Long): User? {
+        return userRepository.findById(id).orElse(null)
+            ?.let { user ->
+                userRepository.delete(user)
+                user
+            }
+    }
+
+
 
 }
